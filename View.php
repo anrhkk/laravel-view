cbaca18565b185c087efa79eb1736fe11955d9dd
anrhkk <anrhkk@sina.com>
2016-04-06 16:20:29 +0800

    Update View.php
    add  function

--- View.php
+++ View.php
@@ -5,6 +5,37 @@ use core\view\Compilers\BladeCompiler;
 use core\view\Engines\CompilerEngine;
 use InvalidArgumentException;
 
+
+if (!function_exists('e')) {
+    /**
+     * Escape HTML entities in a string.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    function e($value)
+    {
+        return htmlentities($value, ENT_QUOTES, 'UTF-8', false);
+    }
+}
+
+if (!function_exists('array_except')) {
+    /**
+     * Get all of the given array except for a specified array of items.
+     *
+     * @param  array  $array
+     * @param  array|string  $keys
+     * @return array
+     */
+    function array_except($array, $keys)
+    {
+        foreach ((array) $keys as $key) {
+            unset($array[$key]);
+        }
+        return $array;
+    }
+}
+
 class View
 {
     /**
